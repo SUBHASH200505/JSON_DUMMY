5f970fb03c3fe7f71ef49519d94c07729edf3f9b
Subhash <163117768+SUBHASH200505@users.noreply.github.com>
2026-04-22 23:08:46 +0530

    extent report updated

--- com.dummy_json/src/test/java/Runner/TestRunner.java
+++ com.dummy_json/src/test/java/Runner/TestRunner.java
@@ -1,13 +1,17 @@
 package Runner;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-        features = "classpath:features",
+        features = "src/test/resources/features",
         glue = {"stepdefinitions", "hooks"},
-        plugin = {"pretty", "html:target/report.html"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-report.html",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
         monochrome = true
 )
-public class TestRunner extends AbstractTestNGCucumberTests{
-	
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
